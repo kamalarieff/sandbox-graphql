17c02c0da80de78b8ff049dc2c78e8349a0cd93d
Kamal Arieff <kamal.arieff@gmail.com>
2019-07-10 23:52:46 +0800

    use sequelize to connect to postgres

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
 import schema from "./schema";
 import resolvers from "./resolvers";
-import models from "./models";
+import models, { sequelize } from "./models";
 
 const app = express();
 
@@ -13,13 +13,15 @@ const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
   context: {
-    models,
-    me: models.users[1]
+    models
+    // me: models.users[1]
   }
 });
 
 server.applyMiddleware({ app, path: "/graphql" });
 
-app.listen({ port: 8000 }, () => {
-  console.log("Apollo Server on http://localhost:8000/graphql");
+sequelize.sync().then(async () => {
+  app.listen({ port: 8000 }, () => {
+    console.log("Apollo Server on http://localhost:8000/graphql");
+  });
 });
